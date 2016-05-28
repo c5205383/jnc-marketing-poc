59b5e58921238459e48f2f8f221f54d7fac55356
Andy He <andy.he01@sap.com>
2016-05-28 15:42:02 +0800

    wechat registration test

--- src/main/java/com/sap/jnc/marketing/web/controllers/WeChatController.java
+++ src/main/java/com/sap/jnc/marketing/web/controllers/WeChatController.java
@@ -39,71 +39,67 @@ public class WeChatController {
 		return wine;
 	}
 
-	@RequestMapping(value = "/wechatadaptor", method = RequestMethod.GET)
-	public void wechatReg(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		String echostr = request.getParameter("echostr");
-		PrintWriter out = response.getWriter();
-		out.print(echostr);
-		out.close();
-		out = null;
-	}
-
-	@RequestMapping(value = "/wechatadaptor", method = RequestMethod.POST)
+	@RequestMapping(value = "/wechatadaptor", method = { RequestMethod.POST, RequestMethod.GET })
 	public void AndyTest(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
 		String currentDate = new Date().toString();
 		System.out.println(currentDate);
 		String result = "";
-		InputStream is = request.getInputStream();
-		StringBuffer sb = new StringBuffer();
-		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-		BufferedReader br = new BufferedReader(isr);
-		String s = "";
-		while ((s = br.readLine()) != null) {
-			sb.append(s);
-		}
-		String xml = sb.toString();
-		System.out.println(xml);
-		ReceiveXmlEntity xmlEntity = new ReceiveXmlProcess().getMsgEntity(xml);
-		if (xmlEntity != null) {
-			String bottelID = "";
-			if ("subscribe".equalsIgnoreCase(xmlEntity.getEvent())) {
-				bottelID = xmlEntity.getEventKey();
-				if (bottelID != null && !"".equals(bottelID.trim())) {
-					if (bottelID.startsWith("qrscene_")) {
-						bottelID = bottelID.replaceFirst("qrscene_", "");
-					} else {
-						bottelID = "";
+		String echostr = request.getParameter("echostr");
+		if (echostr != null && !"".equals(echostr)) {
+			result = echostr;
+		} else {
+			InputStream is = request.getInputStream();
+			StringBuffer sb = new StringBuffer();
+			InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+			BufferedReader br = new BufferedReader(isr);
+			String s = "";
+			while ((s = br.readLine()) != null) {
+				sb.append(s);
+			}
+			String xml = sb.toString();
+			System.out.println(xml);
+			ReceiveXmlEntity xmlEntity = new ReceiveXmlProcess().getMsgEntity(xml);
+			if (xmlEntity != null) {
+				String bottelID = "";
+				if ("subscribe".equalsIgnoreCase(xmlEntity.getEvent())) {
+					bottelID = xmlEntity.getEventKey();
+					if (bottelID != null && !"".equals(bottelID.trim())) {
+						if (bottelID.startsWith("qrscene_")) {
+							bottelID = bottelID.replaceFirst("qrscene_", "");
+						} else {
+							bottelID = "";
+						}
+					}
+				}
+				if ("SCAN".equalsIgnoreCase(xmlEntity.getEvent())) {
+					bottelID = xmlEntity.getEventKey();
+				}
+				if (!"".equals(bottelID.trim())) {
+					Wine wine = wineRepository.findOne(bottelID);
+					if (wine != null) {
+						if (wine.getFlagBonusA() == '0') {
+							result = "谢谢关注剑南春，本次红包金额：" + wine.getAmountA().toString() + "。";
+							wine.setFlagBonusA('1');
+							wineRepository.saveAndFlush(wine);
+						} else {
+							result = "红包已领取，谢谢关注剑南春。";
+						}
+
 					}
 				}
 			}
-			if ("SCAN".equalsIgnoreCase(xmlEntity.getEvent())) {
-				bottelID = xmlEntity.getEventKey();
-			}
-			if (!"".equals(bottelID.trim())) {
-				Wine wine = wineRepository.findOne(bottelID);
-				if (wine != null) {
-					if (wine.getFlagBonusA() == '0') {
-						result = "谢谢关注剑南春，本次红包金额：" + wine.getAmountA().toString() + "。";
-						wine.setFlagBonusA('1');
-						wineRepository.saveAndFlush(wine);
-					} else {
-						result = "红包已领取，谢谢关注剑南春。";
-					}
 
-				}
+			if ("".equals(result.trim())) {
+				result = "谢谢关注剑南春。";
 			}
+			result = new WeChatAutoResponse().response(xml, result);
 		}
-
-		if ("".equals(result.trim())) {
-			result = "谢谢关注剑南春。";
-		}
-		result = new WeChatAutoResponse().response(xml, result);
 		PrintWriter out = response.getWriter();
 		out.print(result);
 		out.close();
 		out = null;
 	}
-	
+
 }
